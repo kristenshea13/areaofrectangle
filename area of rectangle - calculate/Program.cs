0d5a5d338447772f6207dc11d14dbd5c421d18e7
Kristen Shea <kstar61@live.com>
2018-08-29 17:25:30 -0500

    Add project files.

---
+++ area of rectangle - calculate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace area_of_rectangle___calculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
