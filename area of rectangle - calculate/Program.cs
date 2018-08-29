0c5e764409ff9b76633c72552b24131ca0341492
Kristen Shea <kstar61@live.com>
2018-08-29 17:31:28 -0500

    calculate the area of a rectangle from two user-entered variables

--- area of rectangle - calculate/Program.cs
+++ area of rectangle - calculate/Program.cs
@@ -6,7 +6,19 @@ namespace area_of_rectangle___calculate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calculate the area of a rectangle");
+            Console.Write("Enter length of rectangle: ");
+            //var length = Console.ReadLine();
+            int lengthInt = int.Parse(Console.ReadLine());
+            Console.Write("Enter width of rectangle: ");
+            int widthInt = int.Parse(Console.ReadLine());
+
+            int rectangleArea = lengthInt * widthInt;
+
+            Console.WriteLine("The area of your rectangle is: {0}", rectangleArea);
+
+            Console.ReadLine();
+
         }
     }
 }
